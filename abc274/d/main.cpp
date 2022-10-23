06a5b472949b53f84ed0f7ed24ab914f841f171a
shun-yamazaki <30379968+shun-yamazaki@users.noreply.github.com>
2022-10-23 14:10:34 +0900

    abc274: 解説を参考に改善

--- abc274/d/main.cpp
+++ abc274/d/main.cpp
@@ -5,56 +5,42 @@ using namespace std;
 typedef long long ll;
 
 void _main() {
-    int n, x, y, a1;
-    cin >> n >> x >> y >> a1;
-    vector<int> xd(n / 2), yd(n / 2);
-    rep(i, 1, n) {
+    int n, x, y;
+    cin >> n >> x >> y;
+    x += 10000;
+    y += 10000;
+    vector<int> a(n);
+    rep(i, 0, n) cin >> a[i];
+
+    vector<bool> dpx(20001, false), dpy(20001, false);
+    dpx[a[0] + 10000] = true;
+    dpy[10000] = true;
+    for (int i = 1; i < a.size(); i++) {
+        vector<bool> nextdp(20001, false);
         if (i % 2 == 0) {
-            cin >> xd[i / 2];
+            for (int j = 0; j < dpx.size(); j++) {
+                if (dpx[j] && j + a[i] < dpx.size()) {
+                    nextdp[j + a[i]] = true;
+                }
+                if (dpx[j] && j - a[i] >= 0) {
+                    nextdp[j - a[i]] = true;
+                }
+            }
+            dpx = nextdp;
         } else {
-            cin >> yd[i / 2];
+            for (int j = 0; j < dpy.size(); j++) {
+                if (dpy[j] && j + a[i] < dpy.size()) {
+                    nextdp[j + a[i]] = true;
+                }
+                if (dpy[j] && j - a[i] >= 0) {
+                    nextdp[j - a[i]] = true;
+                }
+            }
+            dpy = nextdp;
         }
     }
 
-    bool x_ok = false;
-    for (int bit = 0; bit < (1 << (n / 2)); bit++) {
-        int px = a1;
-        for (int i = 0; i < (n / 2); i++) {
-            if (bit & (1 << i))
-                px += xd[i];
-            else
-                px -= xd[i];
-        }
-        if (px == x) {
-            x_ok = true;
-            break;
-        }
-    }
-    if (!x_ok) {
-        cout << "No" << endl;
-        return;
-    }
-
-    bool y_ok = false;
-    for (int bit = 0; bit < (1 << (n / 2)); bit++) {
-        int py = 0;
-        for (int i = 0; i < (n / 2); i++) {
-            if (bit & (1 << i))
-                py += yd[i];
-            else
-                py -= yd[i];
-        }
-        if (py == y) {
-            y_ok = true;
-            break;
-        }
-    }
-    if (!y_ok) {
-        cout << "No" << endl;
-        return;
-    }
-
-    cout << "Yes" << endl;
+    cout << (dpx[x] && dpy[y] ? "Yes" : "No") << endl;
     return;
 }
 
