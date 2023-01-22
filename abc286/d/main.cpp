6891091a1582ce1de4c22e376c684a7360d7c4f4
shun-yamazaki <30379968+shun-yamazaki@users.noreply.github.com>
2023-01-22 09:36:38 +0900

    abc286: 解説を参考に改善

--- abc286/d/main.cpp
+++ abc286/d/main.cpp
@@ -4,45 +4,26 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 typedef long long ll;
 
-using Coin = map<int, int>;
-struct Dp {
-    bool b;
-    Coin c;
-};
-
 void _main() {
     int n, x;
     cin >> n >> x;
     vector<int> a(n + 1), b(n + 1);
-    int sum = 0;
-    rep(i, 1, n + 1) {
-        cin >> a[i] >> b[i];
-        sum += b[i];
-    }
+    rep(i, 1, n + 1) cin >> a[i] >> b[i];
 
-    vector<vector<Dp>> dp(x + 1, vector<Dp>(sum + 1));
-    rep(i, 1, x + 1) {
-        rep(j, 1, sum + 1) {
-            rep(k, 1, n + 1) {
-                int p = a[k];
-                if (dp[i - p][j - 1].b == true) {
-                    Dp tmp = dp[i - p][j - 1];
-                    if (tmp.c[p] < b[p]) {
-                        dp[i][j].c = tmp.c;
-                        dp[i][j].c[p]++;
-                        dp[i][j].b = true;
-                    }
-                }
+    vector<vector<bool>> dp(n + 1, vector<bool>(x + 1, false));
+    rep(i, 0, n + 1) dp[i][0] = true;
+    rep(i, 1, n + 1) {
+        rep(j, 1, x + 1) {
+            rep(k, 0, b[i] + 1) {
+                if (j < k * a[i]) continue;
+                if (dp[i - 1][j - k * a[i]]) dp[i][j] = true;
             }
         }
     }
-    rep(i, 1, sum + 1) {
-        if (dp[x][i].b == true) {
-            cout << "Yes" << endl;
-            return;
-        }
-    }
-    cout << "No" << endl;
+    if (dp[n][x] == true)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
 
 int main() {
